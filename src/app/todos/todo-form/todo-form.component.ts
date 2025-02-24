c1d20a668bb66cbe52e7d82eebb3df19568e6acf
Vasyl Pashko <vasyl@leadquizzes.co>
2025-02-24 12:03:52 +0200

    Added basic components

---
+++ src/app/todos/todo-form/todo-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.component.scss'
+})
+export class TodoFormComponent {
+
+}
